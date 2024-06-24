3c7b903f5ba979f18106172df895b8685c958f88
gmth6789 <143313748+gmth6789@users.noreply.github.com>
2024-06-24 12:54:24 +0700

    Initial commit

---
+++ index.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const { resolve } = require('path');
+const fs = require('fs');
+
+const app = express();
+const PORT = 8080;
+
+app.use(express.static("./"));
+app.use(bodyParser.urlencoded({
+  extended: true,
+  limit: "200mb"
+}));
+
+app.get('/', (req, res) => {
+  res.sendFile(resolve(__dirname, 'pages/index.html'));
+});
+
+
+app.post('/save.php', (req, res) => {
+  const { file, action, startTemplateUrl, html } = req.body;
+
+  let result = "File saved!";
+  fs.writeFileSync(file, html);
+	
+	try {
+	  fs.writeFileSync(file, html);
+	} catch (err) {
+	  result = "Error saving file!";
+	  console.error(err);
+	}
+
+
+  res.send(result);
+});
+app.listen(PORT, () => {
+  console.log(`App listening at http://localhost:${PORT}`);
+});
